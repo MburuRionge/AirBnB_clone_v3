b71f2063f21c1aab2e876d9180962e77eff24cb9
MburuRionge <victormburu3@gmail.com>
2024-06-14 02:02:55 -0700

    index.html

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,46 @@
 #!/usr/bin/python3
-"""create  a flask app: app_views"""
+"""
+index
+"""
 
 from flask import jsonify
 from api.v1.views import app_views
+
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def api_status():
-    """returns a JSON response for RESTFUL API health"""
-    resp = {'status': "OK"}
-    return jsonify(resp)
-
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    """return: json of all objs"""
-    stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User'),
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
+def status():
+    """
+    status route
+    :return: response with json
+    """
+    data = {
+        "status": "OK"
     }
 
-    return jsonify(stats)
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def stats():
+    """
+    stats of all objs route
+    :return: json of all objs
+    """
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User"),
+    }
+
+    resp = jsonify(data)
+    resp.status_code = 200
+
+    return resp
